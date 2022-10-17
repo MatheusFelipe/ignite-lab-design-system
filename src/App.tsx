0025387c2284a75e31f96aee4f42ee7aaff4b44f
MatheusRodrigues <matheus.felipesbf@gmail.com>
2022-10-16 23:02:15 -0300

    Implemented login interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,61 @@
+import { Envelope, Lock } from 'phosphor-react';
+
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+
 export function App() {
-  return <h1 className="font-bold text-2xl">Lab</h1>;
+  return (
+    <main className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+      <header className="flex flex-col items-center">
+        <Logo />
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça login e comece a usar!
+        </Text>
+      </header>
+      <form className="flex flex-col items-stretch w-full max-w-sm mt-10 gap-4">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input id="email" type="email" placeholder="Digite seu e-mail" />
+          </TextInput.Root>
+        </label>
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input id="password" type="password" placeholder="Digite sua senha" />
+          </TextInput.Root>
+        </label>
+        <label htmlFor="remember" className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Text size="sm" className="text-gray-200">
+            Lembrar de mim por 30 dias
+          </Text>
+        </label>
+        <Button type="submit" className="mt-4">
+          Entrar na plataforma
+        </Button>
+      </form>
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text size="sm" asChild className="text-gray-400 hover:text-gray-200 underline">
+          <a href="#">Esqueceu sua senha?</a>
+        </Text>
+        <Text size="sm" asChild className="text-gray-400 hover:text-gray-200 underline">
+          <a href="#">Não possui conta? Crie uma agora!</a>
+        </Text>
+      </footer>
+    </main>
+  );
 }
